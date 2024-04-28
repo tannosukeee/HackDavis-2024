6783cead51325a1f737a1e6c73e9da2096722ac7
Tan Nguyen <134257009+tannguyen072704@users.noreply.github.com>
2024-04-28 11:00:00 -0700

    Update ReturnDate.java

--- ReturnDate.java
+++ ReturnDate.java
@@ -1,53 +1,37 @@
 package Flight;
 
-import java.util.Scanner;
-
 public class ReturnDate {
-	private int day;
-	private int month;
-	private int year;
+    private int day;
+    private int month;
+    private int year;
 
-	public ReturnDate(int day, int month, int year) {
-		this.day = day;
-		this.month = month;
-		this.year = year;
-	}
+    public ReturnDate(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
 
-	public int getDay() {
-		return day;
-	}
+    public int getDay() {
+        return day;
+    }
 
-	public void setDay(int day) {
-		this.day = day;
-	}
+    public void setDay(int day) {
+        this.day = day;
+    }
 
-	public int getMonth() {
-		return month;
-	}
+    public int getMonth() {
+        return month;
+    }
 
-	public void setMonth(int month) {
-		this.month = month;
-	}
+    public void setMonth(int month) {
+        this.month = month;
+    }
 
-	public int getYear() {
-		return year;
-	}
+    public int getYear() {
+        return year;
+    }
 
-	public void setYear(int year) {
-		this.year = year;
-	}
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int day2 = sc.nextInt();
-		int month2 = sc.nextInt();
-		int year2 = sc.nextInt();
-		
-		while (day2 > 31 || month2 > 12 || year2 > 2026) {
-			System.out.println("Invalid, please enter again.");
-			day2 = sc.nextInt();
-			month2 = sc.nextInt();
-			year2 = sc.nextInt();
-		}
-}
+    public void setYear(int year) {
+        this.year = year;
+    }
 }
